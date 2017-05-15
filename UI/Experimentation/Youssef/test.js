e4d4233746964070532843e04c4debfa71f59957
Youssef Rizkk <youssefarizk@gmail.com>
2017-05-15 17:13:35 +0100

    Copied Ezer files to another directory

--- UI/Experimentation/Youssef/test.js
+++ UI/Experimentation/Youssef/test.js
@@ -1,3 +1,7 @@
 function myFunction(){
-  var vidFrame = document.getElementById("video").src="https://www.youtube.com/embed/b88GynnrTWU";
+  document.getElementById("video").src="https://www.youtube.com/embed/b88GynnrTWU";
+  document.getElementById("showVid2").disabled = true;
+  for (i = 1; i<4; i++){
+    document.getElementById("showVid"+i).disabled = true;
+  }
 }
