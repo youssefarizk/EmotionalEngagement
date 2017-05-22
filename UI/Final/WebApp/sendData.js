8581bd26c2df75f777c678a8113638aa9ec25b2c
Youssef Rizkk <youssefarizk@gmail.com>
2017-05-22 12:31:08 +0100

    Modularizing webjs.js

---
+++ UI/Final/WebApp/sendData.js
@@ -0,0 +1,26 @@
+function sendData(){
+  str = JSON.stringify(dict, null, 4);
+    console.log(str); // Logs output to dev tools console.
+    //document.getElementById('test').innerHTML = (str); // Displays output using window.alert()
+
+
+  $.ajax({
+      url: "https://requestb.in/13a3vxu1",
+      type: "POST",
+      data: str,
+      dataType: "json",
+      success: function (result) {
+          switch (result) {
+              case true:
+                  processResponse(result);
+                  break;
+              default:
+                  resultDiv.html(result);
+          }
+      },
+      error: function (xhr, ajaxOptions, thrownError) {
+      alert(xhr.status);
+      alert(thrownError);
+      }
+  });
+}
