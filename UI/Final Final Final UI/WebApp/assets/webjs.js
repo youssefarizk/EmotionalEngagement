1588f9b5dec8aedf0a91671e3e9f546054057fcd
Youssef Rizkk <youssefarizk@gmail.com>
2017-05-23 16:48:28 +0100

    Changing to newer version

--- UI/Final Final Final UI/WebApp/assets/webjs.js
+++ UI/Final Final Final UI/WebApp/assets/webjs.js
@@ -52,14 +52,16 @@ function sayHello(){
 var testing = document.getElementById('test');
 
 var dict = [];
-var contentDic = [];
+
 var user=document.getElementById('field1');
 function write_rate(valued) {
   dict.push({
-      username:user.value,
-      movieId:vid,
-      content: contentDic
-  });
+    username:user.value,
+    movieId:vid,
+    rate:valued.value,
+    time: player.getCurrentTime(),
+    picuri: picURL
+  })
   dataAcc += String(player.getCurrentTime())+ ", " +valued.value + "\n";
   g2 = new Dygraph(
     document.getElementById("graphdiv2"),
