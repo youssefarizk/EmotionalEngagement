284a7c081fb7722566e0f0232411bd843eca4c41
Anson <anson_tang423@hotmail.com>
2017-05-24 12:52:38 +0100

    split js

--- UI/Final Final Final UI/WebApp/assets/youtube.js
+++ UI/Final Final Final UI/WebApp/assets/youtube.js
@@ -46,24 +46,30 @@ function vid1() {
   player.loadVideoById({'videoId': 'ZK3O402wf1c',
                  'suggestedQuality': 'default'});
                  vid="Lec 1 MIT 18.06 Linear Algebra Spring 2005";
+
+
 }
 function vid2() {
   player.loadVideoById({'videoId': 'mbyG85GZ0PI',
                  'suggestedQuality': 'default'});
                  vid="Lecture 01 - The Learning Problem";
+
 }
 function vid3() {
   player.loadVideoById({'videoId': 'gv-AB35V2k8',
                  'suggestedQuality': 'default'});
                  vid="Lec 1 MIT 18.086 Mathematical Methods for Engineers II";
+
 }
 function vid4() {
   player.loadVideoById({'videoId': 'T_I-CUOc_bk',
                  'suggestedQuality': 'default'});
                  vid="Big Picture: Derivatives";
+
 }
 function vid5() {
   player.loadVideoById({'videoId': 'oo1ZZlvT2LQ',
                  'suggestedQuality': 'default'});
                  vid="The Exponential Function";
+            
 }
