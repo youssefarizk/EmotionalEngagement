ba62836e9d916d44a8c4ce78cf42789ff2f6e9f7
Anson <anson_tang423@hotmail.com>
2017-05-25 14:41:39 +0100

    latest version

--- UI/Final Final Final UI/WebApp/assets/json.js
+++ UI/Final Final Final UI/WebApp/assets/json.js
@@ -23,37 +23,7 @@ var testing = document.getElementById('test');
 var dict = [];
 var user=document.getElementById('field1');
 
-window.onbeforeunload = DOjson;
 
-Dojson = function(){
-  downloadCSV({ filename: "data.csv" });
-
-  str = JSON.stringify(dict, null, 4);
-    console.log(str); // Logs output to dev tools console.
-    //document.getElementById('test').innerHTML = (str); // Displays output using window.alert()
-
-
-  $.ajax({
-      url: "https://requestb.in/137od0r1",
-      type: "POST",
-      data: str,
-      dataType: "json",
-      success: function (result) {
-          switch (result) {
-              case true:
-                  processResponse(result);
-                  break;
-              default:
-                  resultDiv.html(result);
-          }
-      },
-      error: function (xhr, ajaxOptions, thrownError) {
-      alert(xhr.status);
-
-      }
-  });
-
-}
 
 
 function onPlayerStateChange(event) {
