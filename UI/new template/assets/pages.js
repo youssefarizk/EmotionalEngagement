dc70dada79f0eaa01e5924cad696df5f0c5063cc
Anson <anson_tang423@hotmail.com>
2017-06-01 18:36:19 +0100

    NEW

---
+++ UI/new template/assets/pages.js
@@ -0,0 +1,6 @@
+function show1(shown, hidden,hidden1) {
+    document.getElementById(shown).style.display='block';
+    document.getElementById(hidden).style.display='none';
+    document.getElementById(hidden1).style.display='none';
+    return false;
+}
