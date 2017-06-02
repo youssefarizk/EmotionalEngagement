1e9f0487c25880529e348884f04eabec2eac4965
Anson <anson_tang423@hotmail.com>
2017-06-02 14:12:35 +0100

    included video

--- UI/new template/assets/js/pages.js
+++
@@ -1,6 +0,0 @@
-function show1(shown, hidden,hidden1) {
-    document.getElementById(shown).style.display='block';
-    document.getElementById(hidden).style.display='none';
-    document.getElementById(hidden1).style.display='none';
-    return false;
-}
